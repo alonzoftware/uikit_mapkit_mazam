88446fcde88dbd46fcc40f04f8ba66b54689ee0d
MacBookAir <alonzo.choque@icloud.com>
2025-02-26 23:12:42 -0400

    MacBookAir2017 20250226 23:00 :: 01-MapKit-Fundamentals :: End Section

--- 01-MapKit-Fundamentals/HelloMapLon/HelloMap/ViewController.swift
+++ 01-MapKit-Fundamentals/HelloMapLon/HelloMap/ViewController.swift
@@ -11,6 +11,7 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     @IBOutlet weak var mapView : MKMapView!
+    @IBOutlet weak var mapTypeSegmentedControl : UISegmentedControl!
     private let locationManager = CLLocationManager( )
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +22,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         locationManager.distanceFilter = kCLDistanceFilterNone
         locationManager.startUpdatingLocation( )
         self.mapView.showsUserLocation = true
-        
+        self.mapTypeSegmentedControl.addTarget(self, action: #selector(mapTypeChanged), for: .valueChanged)
     }
+    @objc func mapTypeChanged(segmentedControl: UISegmentedControl){
+        switch(segmentedControl.selectedSegmentIndex){
+        case 0:
+            self.mapView.mapType = .standard
+        case 1:
+            self.mapView.mapType = .satellite
+        case 2:
+            self.mapView.mapType = .hybrid
+        default:
+            self.mapView.mapType = .standard
+        }
+    }
+    
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         //        let region = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
         let region = MKCoordinateRegion(center: userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.008, longitudeDelta: 0.008))
