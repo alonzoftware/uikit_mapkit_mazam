3b4a3501a0827b53979dfbfc669014e08aed8d27
MacBookAir2019 <alonzo.choque@icloud.com>
2025-03-19 20:29:55 -0400

    MacBookAir2019 20250319 08:29 Add Custom Annotations with Image, Emoji and Letters

---
+++ 02-Annotations/MapKit-Annotations-Lon/MapKit-Annotations-Lon/CoffeeAnnotation.swift
@@ -0,0 +1,11 @@
+//
+//  CoffeeAnnotation.swift
+//  MapKit-Annotations-Lon
+//
+//  Created by MacBookAir2019 on 19/3/25.
+//
+import Foundation
+import MapKit
+class CoffeeAnnotation : MKPointAnnotation{
+    var imageURL : String!
+}
--- 02-Annotations/MapKit-Annotations-Lon/MapKit-Annotations-Lon/ViewController.swift
+++ 02-Annotations/MapKit-Annotations-Lon/MapKit-Annotations-Lon/ViewController.swift
@@ -10,7 +10,7 @@ import MapKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
-
+    
     @IBOutlet weak var mapView : MKMapView!
     @IBOutlet weak var mapTypeSegmentedControl : UISegmentedControl!
     private let locationManager = CLLocationManager( )
@@ -26,12 +26,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         self.mapTypeSegmentedControl.addTarget(self, action: #selector(mapTypeChanged), for: .valueChanged)
     }
     @IBAction func addAnnotationButtonPressed() {
-       let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(latitude: -16.47660816416993, longitude: -68.12385957980739)
+        let annotation = MKPointAnnotation()
+        //Close VFatima Home
+        //annotation.coordinate = CLLocationCoordinate2D(latitude: -16.47660816416993, longitude: -68.12385957980739)
+        //Close AXS
+        annotation.coordinate = CLLocationCoordinate2D(latitude:-16.54063096964433 , longitude:  -68.08032941865261)
         annotation.title = "Ahu Tongariki"
         annotation.subtitle = "Fiordland National Park"
         self.mapView.addAnnotation(annotation)
     }
+    
+    @IBAction func addCustomAnnotationButtonPressed() {
+        let annotation = CoffeeAnnotation()
+        //Close VFatima Home
+        //annotation.coordinate = CLLocationCoordinate2D(latitude: -16.47660816416993, longitude: -68.12385957980739)
+        //Close AXS
+        annotation.coordinate = CLLocationCoordinate2D(latitude:-16.540836489467605 , longitude:  -68.08133368818132)
+        annotation.title = "Ahu Tongariki"
+        annotation.subtitle = "Fiordland National Park"
+        annotation.imageURL = "coffee-pin"
+        self.mapView.addAnnotation(annotation)
+    }
+    
     @objc func mapTypeChanged(segmentedControl: UISegmentedControl){
         switch(segmentedControl.selectedSegmentIndex){
         case 0:
@@ -44,12 +60,60 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             self.mapView.mapType = .standard
         }
     }
-    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        print("Annotation: \(annotation.self)")
+        if annotation is MKUserLocation {
+            return nil
+        }
+       //FIRST OPTION - IMAGE
+//        var coffeeAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "CoffeeAnnotationView")
+//        
+//        if coffeeAnnotationView == nil {
+//            
+//            coffeeAnnotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "CoffeeAnnotationView")
+//            coffeeAnnotationView?.canShowCallout = true
+//        } else {
+//            coffeeAnnotationView?.annotation = annotation
+//        }
+//        
+//        if let coffeeAnnotation = annotation as? CoffeeAnnotation {
+//            coffeeAnnotationView?.image = UIImage(named: coffeeAnnotation.imageURL)
+//        }else{
+//            return nil
+//        }
+//        
+//        return coffeeAnnotationView
+        
+        //SECOND OPTION - EMOJI
+        var coffeeAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "CoffeeAnnotationView") as? MKMarkerAnnotationView
+        
+        if coffeeAnnotationView == nil {
+            
+            coffeeAnnotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "CoffeeAnnotationView")
+//            coffeeAnnotationView?.glyphText = "😃"
+            coffeeAnnotationView?.glyphText = "B"
+            coffeeAnnotationView?.markerTintColor=UIColor.purple
+            coffeeAnnotationView?.glyphTintColor = UIColor.yellow
+            
+            coffeeAnnotationView?.canShowCallout = true
+        } else {
+            coffeeAnnotationView?.annotation = annotation
+        }
+        
+        if annotation is CoffeeAnnotation {
+        }else{
+            return nil
+        }
+        
+        return coffeeAnnotationView
+        
+        
+    }
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         //        let region = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
         let region = MKCoordinateRegion(center: userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.008, longitudeDelta: 0.008))
         self.mapView.setRegion(region, animated: true)
     }
-
+    
 }
 
